ee83fe7e834270393070c7e9825cbe817310ad7e
Markus Stein <mstein@shivi.de>
2021-01-20 13:41:13 +0100

    Initial commit

---
+++ PlayAudio/Program.cs
@@ -0,0 +1,63 @@
+﻿using NAudio.Wave;
+using System;
+using System.Threading;
+
+namespace PlayAudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("PlayAudio v1.0");
+                Console.WriteLine();
+                Console.WriteLine("PlayAudio <SoundFile> <AudioDevice>");
+                foreach (var dev in DirectSoundOut.Devices)
+                {
+                    Console.WriteLine($"{dev.Description} --- GUID: {dev.Guid}");
+                }
+                return;
+            }
+
+            string audioFileName = args[0];
+            string DeviceName = args[1];
+
+            Guid deviceGuid = new Guid();
+            foreach (var dev in DirectSoundOut.Devices)
+            {
+                if (dev.Description.Contains(DeviceName))
+                {
+                    deviceGuid = dev.Guid;
+                    Console.WriteLine($"{dev.Guid} {dev.ModuleName} -  {dev.Description}");
+                    break;
+                }
+            }
+
+            // nach GUID suchen
+            if (deviceGuid.ToString() == "00000000-0000-0000-0000-000000000000")
+            {
+                foreach (var dev in DirectSoundOut.Devices)
+                {
+                    if (dev.Guid.ToString().Contains(DeviceName))
+                    {
+                        deviceGuid = dev.Guid;
+                        Console.WriteLine($"{dev.Guid} {dev.ModuleName} -  {dev.Description}");
+                        break;
+                    }
+                }
+            }
+            
+            using (var audioFile = new AudioFileReader(audioFileName))
+            using (var outputDevice = new DirectSoundOut(deviceGuid))
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
